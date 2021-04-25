51aab9d050b34d6586d95c3d7d6378a2fb29d432
Laxman Bista <bistalaxman5@gmail.com>
2021-04-24 19:40:42 -0500

    Initializing

---
+++ src/main/java/com/java/nolimitjava/NoLimitJavaApplication.java
@@ -0,0 +1,13 @@
+package com.java.nolimitjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoLimitJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoLimitJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java/nolimitjava/NoLimitJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.nolimitjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoLimitJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
